fa216c291ddc68b493831bc10a393ef18edfbe0a
Gregor Wolf <gregor.wolf@gmail.com>
2024-03-27 10:18:03 +0100

    custom handler for Books update

--- srv/review-service.js
+++ srv/review-service.js
@@ -1,3 +1,5 @@
+const cds = require("@sap/cds");
+const LOG = cds.log("review-service");
 // Read xsappname of services using xsenv
 const xsenv = require("@sap/xsenv");
 xsenv.loadEnv();
@@ -9,10 +11,18 @@ const services = xsenv.getServices({
 });
 
 module.exports = cds.service.impl(async function () {
+  const db = await cds.connect.to("db");
+  const { Books: Books } = db.entities;
+
   this.on("getBookshopServiceManager", (req) => {
     return services.smbookshop;
   });
+
   this.on("getServiceBindingForTenantId", (req) => {
     return getServiceBindingForTenantId(req.data.tenantId);
+  });
+
+  this.on("UPDATE", "Books", async (req) => {
+    LOG.debug("Books UPDATE request received");
   });
 });
